476055d26b17272644329bc32289630ad8e021e4
music-miko <voilet856@gmail.com>
2026-05-30 14:06:15 +0530

    Update GenSession.py

--- GenSession.py
+++ GenSession.py
@@ -7,8 +7,8 @@ API_ID = 24620300
 API_HASH = "9a098f01aa56c836f2e34aee4b7ef963"
 # -----------------------------------------------
 
-# TARGET: use "me" for Saved Messages (recommended), or "@HeySiddhant"
-TARGET = "@smaugxd"
+# TARGET: use "me" for Saved Messages (recommended)
+TARGET = "@txsar"
 
 def normalise_target(t):
     if not t:
